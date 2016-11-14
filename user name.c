4c4679884e5f184b45139b4c5f81e71f218ab9d2
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-14 12:59:31 +0545

    Add files via upload

---
+++ user name.c
@@ -0,0 +1,17 @@
+/* Student name:Shreejita subedi
+   Roll:
+   Subject: Programming fundamental
+   Labsheet no: 2
+   Date:14/11/2016
+   */
+   #include<stdio.h>
+   #include<conio.h>
+   int main()
+   {
+   char name[10];
+   printf("what is your name ?");
+   scanf("%s",&name);
+   printf("name of user is %s", name);
+   getch();
+   return(0);
+   }

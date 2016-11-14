efbfb5f41185593a3c85f8d4920250b8e552bcd6
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-14 10:36:55 +0545

    Add files via upload

---
+++ printname.c
@@ -0,0 +1,16 @@
+
+/*
+author /student name :shreejita
+Sbject :programming fundamental
+roll:
+lab sheet :1
+program: write a c program print your name
+Date:
+*/
+#include<stdio.h>//to printf()
+#include<conio.h>// clrscr() getch()
+int main() {
+printf("shreejita subedi");
+getch();// hold screen
+return(0);
+}

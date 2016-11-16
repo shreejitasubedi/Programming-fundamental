77411297bd95ffc6583dd50e8fa5e5653c4dd9e8
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-16 10:50:49 +0545

    Add files via upload

---
+++ product.c
@@ -0,0 +1,21 @@
+/*
+sutdent name:shreejita subedi
+roll:
+labsheet: 02
+program : write a program to print a product of two number
+date:16/11/2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main() {
+int a,b,pro;
+printf("enter first number\n");
+scanf("%d",&a);
+printf("enter second number\n");
+scanf("%d",&b);
+ pro=a*b;
+ printf("the product of number is:%d",pro);
+ getch();
+ return(0);
+ }

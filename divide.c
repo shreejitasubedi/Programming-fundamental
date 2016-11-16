0750973f336ab89c00a41c48cc454f72206ef71a
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-16 12:28:49 +0545

    Add files via upload

---
+++ divide.c
@@ -0,0 +1,17 @@
+/*
+student name:shreejita subedi
+lab sheet:02
+program:wap to pirnt divide of two number
+date:16/11/2016
+*/
+int main (){
+float a,b,div;
+printf("enter the first number:\n");
+scanf("%f",&a);
+printf("enter the second number :\n");
+scanf("%f",&b);
+div=(a/b);
+printf("the division of two number is:%f",div);
+getch();
+return(0);
+}

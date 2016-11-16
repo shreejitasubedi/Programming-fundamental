d04ad5aca0479644992fb2e888dc81cd76f41975
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-16 12:51:47 +0545

    Add files via upload

---
+++ area of t.c
@@ -0,0 +1,17 @@
+/*
+student name:shreejita subedi
+lab sheet : 02
+program :wpa to print area of triangle
+date :16/11/2016
+*/
+int main (){
+float b,h,area=0;
+printf("Enter the first number:\n");
+scanf("%f",&b);
+printf("Enter the second number:\n");
+scanf("%f",&h);
+area=(0.5)*b*h;
+printf("the area of triangle is:%f",area);
+getch();
+return(0);
+}

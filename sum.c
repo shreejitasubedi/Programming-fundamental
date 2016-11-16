c2dcc31eedd3762ac18e7d6c844929c94bb3a1e9
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-16 10:51:08 +0545

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,20 @@
+/*
+student name: shreejita subedi
+roll:
+subject:porgramming fundamental
+program:WAP to pirnt sum of two no asked from user
+*/
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int a,b,sum;
+printf("enter first number:\n");
+scanf("%d",&a);
+printf("enter the second number:\n");
+scanf("%d",&b);
+sum=a+b;
+printf("sum of two number is:%d",sum);
+getch();
+
+}

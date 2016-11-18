acb018a0757c99d8a05dda8c7475fe5d92ce5bc7
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-18 13:00:22 +0545

    Add files via upload

--- circle.c
+++ circle.c
@@ -6,12 +6,13 @@ program:WAP to print area of circle  & circumference
 */
 #include<stdio.h>
 #include<conio.h>
-
+#include<math.h>
+#define PI 3.14
 int main (){
-float r,area,cir,PI=3.14;
+float r,area,cir;
 printf("enter the number:\n");
 scanf("%f",&r);
-area=PI*r*r;
+area=PI*pow(r,2);
 cir=2*PI*r;
 printf("the area of circle is:%f\n",area);
 printf("the area of circumference is :%f",cir);

db70579c4297ae708bc8d92f62f7a272418b21ad
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-21 14:37:17 +0545

    Add files via upload

---
+++ trepizum.c
@@ -0,0 +1,23 @@
+/*
+STUDENT : SHREEJIYA SUBEDI
+ROLL:
+Subject: programming fundamental
+Lab sheet :4
+*/
+#include<stdio.h>
+#include<conio.h>
+int main (){
+float a,b,h,area;
+printf("Enter the first number:\n");
+scanf("%f",&a);
+printf("Enter the second number;\n");
+scanf("%f",&b);
+printf("Enter the height;\n");
+scanf("%f",&h);
+area=0.5*(a+b)*h;
+printf("The area of trapezium is:%f",area);
+getch();
+return(0);
+}
+
+

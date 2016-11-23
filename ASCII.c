e72b8d60bec6330f16ae80fa2d9fa81e04dc2d32
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-23 12:41:21 +0545

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,22 @@
+/*
+student : shreejita subedi
+roll:
+program:WAP to pirnt ASCII
+lab sheet :5
+*/
+#include<stdio.h>
+#include<conio.h>
+int main (){
+char character;
+int asciivalue;
+printf("enter a character :");
+scanf("%c",&character);
+asciivalue=character;
+printf("the character is:%d",asciivalue);
+getch();
+return(0);
+
+
+
+
+}

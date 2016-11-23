961433613e33352b8055d533cb5a15b8f339dc16
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-23 12:58:30 +0545

    Add files via upload

--- root.c
+++ root.c
@@ -8,18 +8,18 @@ labsheet :5
 #include<conio.h>
 #include<math.h>
 int main(){
-float x,y,a,b,c,dis;
+float x=0,y=0,a,b,c,dis=0;
 printf("enter the first number :\n");
 scanf("%f",&a);
 printf("enter the second number:\n");
 scanf("%f",&b);
-print("enter the third number :\n");
+printf("enter the third number :\n");
 scanf("%f",&c);
 dis=(b*b)-(4*a*c);
-x=pow(dis,0.5)/(2*a);
-y=-pow(dis,0.5)/(2*a);
-printf("the quadratic equation are %f",x);
-printf("the quadratic equatation are %f",y);
+x=(-b-pow(dis,0.5))/(2*a);
+y=(-b+pow(dis,0.5))/(2*a);
+printf("the quadratic equation are %f\n",x);
+printf("the quadratic equation are %f",y);
 getch();
 return(0);
 }

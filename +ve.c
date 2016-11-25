18b246e531b633f77505664f0f83da0280cf6a4f
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-25 10:30:36 +0545

    Add files via upload

---
+++ +ve.c
@@ -0,0 +1,24 @@
+/*
+student name: shreejita subedi
+Roll:
+program: WAP to check positive number
+Date :25,nov,2016
+Subject : programming fundamental
+*/
+#include<stdio.h>
+#include<conio.h>
+int main (){
+int n;
+printf("Enter the number:\n");
+scanf("%d",&n);
+if(n>0)
+    {
+        printf("The number is positive\n");
+    }
+    else
+    {
+        printf("The number is negative\n");
+    }
+
+    return(0);
+}

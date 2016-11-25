6a970dad23a103dfe1a4d5ca0ec7378b73f2126c
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-25 10:54:56 +0545

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,23 @@
+/*
+student : shreejita subedi
+Roll:
+program: WAP to check odd or even
+Date:25,nov2016
+*/
+#include<stdio.h>
+
+int main(){
+int n;
+printf("Enter the number :\n");
+scanf("%d",&n);
+
+if (n%2==0)
+{
+printf("The number is even");
+}
+else
+{
+    printf("The number is odd");
+}
+return(0);
+}

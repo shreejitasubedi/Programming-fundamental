286bbb2c345c75fc186d8beb0b601eb9f4e8a4a1
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-28 10:46:26 +0545

    Add files via upload

---
+++ marksheet..c
@@ -0,0 +1,33 @@
+/*
+Student name :Shreejita subedi
+Roll:
+Subject:programming fundamental
+Date :28,nov2016
+lABSHEET:7
+Program:WAP to enter the marks of the subject
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float CA,OM,A,IS,PF,SE,total,average,percentage;
+printf("Enter the marks in computer architecture:");
+scanf("%f",&CA);
+printf("Enter the marks in organization management:");
+scanf("%F",&OM);
+printf("Enter the marks in algebra:");
+scanf("%f",&A);
+printf("Enter the marks in interactive skill:");
+scanf("%f",&IS);
+printf("Enter the marks in programming fundamental:");
+scanf("%f",&PF);
+printf("Enter the marks in software engineering:");
+scanf("%f",&SE);
+total=CA+OM+A+IS+PF+SE;
+average=total/6;
+percentage=total/600*100;
+printf("The total marks is :%f\n",total);
+printf("The average marks is :%f\n",average);
+printf("The percentage is :%f\n",percentage);
+getch();
+return(0);
+}

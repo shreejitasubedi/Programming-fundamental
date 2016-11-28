514fd4e14dfc795538a0d98a8cb9650e3e451bad
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-28 11:10:47 +0545

    Add files via upload

---
+++ relational.c
@@ -0,0 +1,30 @@
+/*
+Roll:
+Subject:programming fundamental
+Date :28,nov2016
+lABSHEET:7
+Program:WAP to use relational operator
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+a==b;
+a>b;
+a<b;
+a!=b;
+a>=b;
+a<=b;
+printf("The use of ==:%d\n",a==b);
+printf("The uses of >:%d\n",a>b);
+printf("The use of <:%d\n",a<b);
+printf("The use of !=:%d\n",a!=b);
+printf("The use of>=:%d\n",a>=b);
+printf("The use of <=:%d\n",a<=b);
+getch();
+return(0);
+}

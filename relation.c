87e22b9d88b36e6d038776a937d02b2db3f1c5e2
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-11-29 23:18:11 +0545

    Add files via upload

---
+++ relation.c
@@ -0,0 +1,21 @@
+
+
+	#include<stdio.h>
+	#include<conio.h>
+	int main(){
+	int a,b;
+	printf("Enter the first number:");
+	scanf("%d",&a);
+	printf("Enter the second number:");
+	scanf("%d",&b);
+	printf("The use of ==:%d\n",a==b);
+	printf("The uses of >:%d\n",a>b);
+	printf("The use of <:%d\n",a<b);
+	printf("The use of !=:%d\n",a!=b);
+	printf("The use of>=:%d\n",a>=b);
+	printf("The use of <=:%d\n",a<=b);
+	getch();
+	return(0);
+	}
+
+

0b7d7f8edfefe702dc94cc29c6fbce8acd2e0b7f
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-12-07 11:19:06 +0545

    Add files via upload

---
+++ greaterrrr.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,c;
+printf("Enter a number :\n");
+scanf("%d",&a);
+printf("Enter second number:\n");
+scanf("%d",&b);
+printf("Enter third number :\n");
+scanf("%d",&c);
+if (a>=b&&a>=c)
+{
+    printf("The largest is :%d",a);
+}
+else if(b>=c)
+{
+    printf("The largest is  :%d",b);
+
+}
+else
+{
+    printf("The largest is:%d",c);
+}
+getch();
+return(0);
+}

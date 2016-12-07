f8316dd8c3a4055006ecea4794326739ea5ee908
shreejitasubedi <shreejita.subedi89@gmail.com>
2016-12-07 11:48:28 +0545

    Add files via upload

---
+++ ternaryoddeven.c
@@ -0,0 +1,14 @@
+/*
+name :shreejita subedi
+roll:37
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int n;
+printf("Enter a number :");
+scanf("%d",&n);
+(n%2==0)?printf("The entered number number %d  is even",n):printf("The entered number %d is odd",n);
+getch();
+return (0);
+}

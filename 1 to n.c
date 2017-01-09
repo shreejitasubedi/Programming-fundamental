d39adca248425879abbb18df4f870c75bbeeb06f
shreejitasubedi <shreejita.subedi89@gmail.com>
2017-01-09 11:17:01 +0545

    Add files via upload

---
+++ 1 to n.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main (){
+int n,i;
+printf("Enter the number :");
+scanf("%d",&n);
+i=1;
+while (i<=n)
+    {
+        printf("%d",i);
+        i++;
+    }
+    getch ();
+    return 0;
+
+}

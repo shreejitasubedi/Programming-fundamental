9e7ba0b704d131d1c41022d8ee4b82960a39e3e6
shreejitasubedi <shreejita.subedi89@gmail.com>
2017-01-16 12:27:52 +0545

    Add files via upload

--- count.c
+++ count.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main () {
-int n,rev,reverse=0;
+int n,count=1;
 
 	printf("enter the number:");
 	scanf("%d",&n);
 	
 	while (n>0)
 	{
-		rev=n%10;
-		reverse=reverse*10+rev;
+	n=n/10;
+	count=count+1;
 	    n=n/10;
 	}
-	printf("the revese is :%d",reverse);
+	printf("the digit :%d",count);
  
 		return 0;
 }
---
+++ untitled.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main () {
+	int i,j;
+	
+
+for(i=1;i<=2;i++)
+{
+	for(j=1;j<=2;j++)
+	{
+		
+	printf("1");	
+
+	}
+	printf("\n");
+}
+	return 0;
+
+}
